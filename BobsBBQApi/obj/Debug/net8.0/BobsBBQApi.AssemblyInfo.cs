075a092a1bce4d96a732f53bd17a94e887cf9a26
Malene1603 <143804625+Malene1603@users.noreply.github.com>
2025-05-13 21:04:02 +0200

    Frontend changes

--- BobsBBQApi/obj/Debug/net8.0/BobsBBQApi.AssemblyInfo.cs
+++ BobsBBQApi/obj/Debug/net8.0/BobsBBQApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BobsBBQApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee642e7d6142b50930467249d63d73aa1645e293")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+278d4fa115f4aabf1ff43c590e86d46422ee534c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BobsBBQApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BobsBBQApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a7a1ebcf6fae83564b52a0afe282060b737d95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+278d4fa115f4aabf1ff43c590e86d46422ee534c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
