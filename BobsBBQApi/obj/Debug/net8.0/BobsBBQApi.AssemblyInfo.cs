5f5ffcc95f64019f30cdef8674f25716e1763ef9
Malene1603 <143804625+Malene1603@users.noreply.github.com>
2025-05-20 15:02:32 +0200

    Resever bord virker sådan næsten måske

--- BobsBBQApi/obj/Debug/net8.0/BobsBBQApi.AssemblyInfo.cs
+++ BobsBBQApi/obj/Debug/net8.0/BobsBBQApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BobsBBQApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e79534e9c4834a1b42f14859319c038b6aa9074")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de6469de0cf02f7b51da4bd1c09efdb5668c4bf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BobsBBQApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BobsBBQApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
