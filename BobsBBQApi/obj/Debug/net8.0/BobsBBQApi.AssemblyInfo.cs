1e0ac701f7c650b79104a37d4df8daeb801040a8
Jakoc <jacand01@easv365.dk>
2025-05-25 13:24:35 +0200

    dd

--- BobsBBQApi/obj/Debug/net8.0/BobsBBQApi.AssemblyInfo.cs
+++ BobsBBQApi/obj/Debug/net8.0/BobsBBQApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BobsBBQApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17d070fb2e157fd789badef50effb658507ff26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ece268316e3b8752b999fecb223824906117e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BobsBBQApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BobsBBQApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17d070fb2e157fd789badef50effb658507ff26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ece268316e3b8752b999fecb223824906117e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
