ed3ca46c1bc6256c2e98624eaad30817625e26ea
Jakoc <jacand01@easv365.dk>
2025-05-26 19:05:47 +0200

    cc

--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ccac4a3c4419aaee763aec7f27fe79cf5866b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bf690619341e7c75040517be079609aec2bb19c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
